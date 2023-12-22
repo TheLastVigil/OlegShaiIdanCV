82705692155caff30ab00149a34d4f9b24b8aab1
TheLastVigil <idanflishman@gmail.com>
2023-12-22 16:16:30 +0200

    input search filter

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const x = 7;
-console.log(x);
+const ul = document.querySelector('ul');
+const lis = ul.querySelectorAll('li');
+const searchInput = document.querySelector('input[type="search"]');
+
+searchInput.addEventListener('input', () => {
+    lis.forEach((li) => {
+        if (!li.innerHTML.toLowerCase().includes(searchInput.value.toLowerCase())) {
+            li.style.display = 'none';
+        } else {
+            li.style.display = 'block';
+        }
+    });
+});
